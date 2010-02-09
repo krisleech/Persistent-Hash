afb056e367346eb0b1a1b0f41cda73d7f4998426
Kris Leech <kris.leech@interkonect.com>
2010-02-09 14:39:51 +0000

    Adjust README

--- lib/phash.rb
+++ lib/phash.rb
@@ -1,16 +1,16 @@
 class Database < Hash
-	def initialize
-		super
-		@filename = self.class.to_s + '.yml'
-		File.open(@filename, 'w') { |f| f.write({}.to_yaml) } unless File.exists? @filename		
-  	self.merge!(YAML::load(File.read(@filename)))
-	end
-	
-	def filename
-	  @filename
+  def initialize
+    super
+    @filename = self.class.to_s + '.yml'
+    File.open(@filename, 'w') { |f| f.write({}.to_yaml) } unless File.exists? @filename   
+    self.merge!(YAML::load(File.read(@filename)))
   end
-		
-	def save
-	  File.open(@filename, 'w') { |f| f.write(self.to_yaml) }
+  
+  def filename
+    @filename
+  end
+    
+  def save
+    File.open(@filename, 'w') { |f| f.write(self.to_yaml) }
   end
 end  
